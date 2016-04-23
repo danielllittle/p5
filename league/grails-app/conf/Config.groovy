d94991d77c19a4d8cbf5cdbd852be52945c366cf
Daniel Little <little@fas.harvard.edu>
2016-04-23 12:42:19 -0700

    added spring sec core plugin + dbconsole exception

--- league/grails-app/conf/Config.groovy
+++ league/grails-app/conf/Config.groovy
@@ -115,3 +115,19 @@ log4j.main = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'cscie56.ps5.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'cscie56.ps5.UserRole'
+grails.plugin.springsecurity.authority.className = 'cscie56.ps5.Role'
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+        '/':                ['permitAll'],
+        '/index':           ['permitAll'],
+        '/index.gsp':       ['permitAll'],
+        '/assets/**':       ['permitAll'],
+        '/**/js/**':        ['permitAll'],
+        '/**/css/**':       ['permitAll'],
+        '/**/images/**':    ['permitAll'],
+        '/**/favicon.ico':  ['permitAll'],
+        '/dbconsole/**':  	['permitAll']
+]
